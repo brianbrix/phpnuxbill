5e0d1007c9ef4c67bf4f1ccc85c01e42337f1cce
brianbrix <mokandubrian@gmail.com>
2026-02-23 10:40:30 +0300

    config

--- config.php
+++ config.php
@@ -12,11 +12,11 @@ define('APP_URL', $protocol . $host . $baseDir);
 
 $_app_stage = 'Live'; # Do not change this
 
-$db_host    = "localhost"; # Database Host
-$db_port    = "";   # Database Port. Keep it blank if you are un sure.
-$db_user    = "root"; # Database Username
-$db_pass    = ""; # Database Password
-$db_name    = "phpnuxbill"; # Database Name
+$db_host    = "db"; # Database Host
+$db_port    = "3306";   # Database Port. Keep it blank if you are un sure.
+$db_user    = "nuxbill"; # Database Username
+$db_pass    = "myrootpassword"; # Database Password
+$db_name    = "nuxbill"; # Database Name
 
 $config['site_url'] = 'https://hotspot.afyaquik.com/';
 
