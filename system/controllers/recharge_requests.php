15cb58ed714f624f8bfc435d3eea707b0fbf97dd
brianbrix <mokandubrian@gmail.com>
2026-02-25 22:11:13 +0300

    Add recharge request to revenue

--- system/controllers/recharge_requests.php
+++ system/controllers/recharge_requests.php
@@ -69,21 +69,34 @@ switch ($action) {
         $router_name = ($bill && !empty($bill['routers'])) ? $bill['routers'] : $plan['routers'];
 
         if (Package::rechargeUser($customer['id'], $router_name, $plan['id'], 'Admin', 'Recharge Request')) {
+            // Fetch the transaction just created by rechargeUser (same as admin manual recharge flow)
+            $in = ORM::for_table('tbl_transactions')
+                ->where('username', $customer['username'])
+                ->order_by_desc('id')
+                ->find_one();
+
+            // Generate invoice PDF/record (same as admin manual recharge)
+            if ($in) {
+                Package::createInvoice($in);
+            }
+
             $request->status = 'completed';
             $request->admin_id = $admin['id'];
             $request->admin_response = 'Recharged successfully by admin';
             $request->processed_date = date('Y-m-d H:i:s');
             $request->save();
-            
-            // Send notification to customer
-            Message::sendInvoice($customer, null);
-            Message::sendTelegram("#u" . $customer['username'] . " (#id" . $customer['id'] . ") #recharge #auto\n" .
+
+            // Send invoice notification to customer
+            Message::sendInvoice($customer, $in ?: null);
+            Message::sendTelegram(
+                "#u" . $customer['username'] . " (#id" . $customer['id'] . ") #recharge #approved\n" .
                 "Plan: " . $plan['name_plan'] . "\n" .
                 "Price: " . Lang::moneyFormat($plan['price']) . "\n" .
-                "Admin: " . $admin['username']);
-            
-            _log('Admin ' . $admin['username'] . ' approved recharge request #' . $id . ' for customer ' . $customer['username'], 'Admin', $admin['id']);
-            
+                "Admin: " . $admin['username']
+            );
+
+            _log('[' . $admin['username'] . ']: Recharge ' . $customer['username'] . ' [' . $plan['name_plan'] . '][' . Lang::moneyFormat($plan['price']) . ']', $admin['user_type'], $admin['id']);
+
             r2(getUrl('recharge_requests/list'), 's', Lang::T('Recharge request approved and processed'));
         } else {
             r2(getUrl('recharge_requests/view/' . $id), 'e', Lang::T('Failed to process recharge'));
